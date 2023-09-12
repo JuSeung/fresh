411094f7b593e4a5d1747e469e1dc4cd3a7ce13e
juseung <jsshin6246@gmail.com>
2023-09-12 16:58:43 +0900

    useState  사용법
    리액트에서 array/object로 되어있는 state를 수정하고 싶으면
    
    독립적인 카피본을 만들어서 수정해야합니다.
    
    [...기존state]
    
    {...기존state}
    
    이렇게 하면 독립적인 카피가 하나 생성됩니다.
    
    문자나 숫자로 되어있는 state는 굳이 그럴 필요 없습니다.

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,18 +1,32 @@
 import {age, name, hello} from './data.js'; // ./(현재경로) ../(상위폴더)
 
 export default function Cart() {
+    let carts = ['Tomatoes', 'Pasta'];
     return (
       <div>
-      {hello}
-      <h4 className="title">Cart</h4>
-        <CartItem/>
+        {hello}
+        <h4 className="title">Cart</h4>
+        <CartItem item={carts[0]}/>
+        <Banner content="롯데카드"/>
+        <Banner content="현대카드"/>
+        <Btn color="red"/>
+        <Btn color="blue"/>
       </div> 
     )
   }
-  function CartItem() {
+  
+  function Btn(props){
+    return <button style={{ background : props.color }}>버튼임</button>
+  }
+
+  function Banner(props) {
+    return <h5>{props.content} 결제 행사중</h5>
+  }
+
+  function CartItem(props) {
     return (
       <div className="cart-item">
-        <p>상품명 {age}</p>
+        <p>{props.item}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
--- app/list/page.js
+++ app/list/page.js
@@ -1,16 +1,40 @@
+'use client'
+
+import { useState } from "react";
 
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Cocount'];
+    // state : 데이터 잠깐 저장
+    // 장점 : state 변경되면 사용하고 있는 html 자동 랜더링 된다.
+    // state 자주쓰면 좋지만 굳이 안바뀌는 데이터는 안해도 된다.
+    let [수량, 수량변경] = useState([0,0,0]);
 
     return (
         <div>
           <h4 className="title"> 상품목록 </h4>
+            {/* <span>{수량[0]}</span>
+            <button onClick={() => {
+                let copy = [...수량];
+                copy[0] ++;
+                수량변경(copy);
+            }}> + </button> */}
             {
                 상품.map((a, i) => {
                     return (
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} className="food-img"></img>
-                            <h4>{a} $40 </h4>    
+                            <h4>{a} $40 </h4>
+                            <span> {수량[i]} </span>
+                            <button onClick={() => {
+                                    let plusCopy = [...수량];
+                                    plusCopy[i]++;
+                                    수량변경(plusCopy);
+                            }}>+</button>
+                            <button onClick={() => {
+                                    let minusCopy = [...수량];
+                                    minusCopy[i]--; 
+                                    수량변경(minusCopy)
+                            }}>-</button>
                         </div>
                     )
                 })
